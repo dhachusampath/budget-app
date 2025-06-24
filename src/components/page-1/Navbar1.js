8331b23f5343e4634c10a904d6043a7b9554dcba
gitdharshan <your-email@example.com>
2025-06-24 16:25:02 +0530

    first commit

--- src/components/page-1/Navbar1.js
+++ src/components/page-1/Navbar1.js
@@ -1,24 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar1 = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
     <nav className="navbar">
-      <div className="hamburger" onClick={toggleMenu}>
-        ☰
-      </div>
-      <ul className={`nav-list ${isOpen ? 'open' : ''}`}>
-        <li><NavLink to="/" onClick={() => setIsOpen(false)}>Home</NavLink></li>
-        <li><NavLink to="/about" onClick={() => setIsOpen(false)}>About Us</NavLink></li>
-        <li><NavLink to="/why" onClick={() => setIsOpen(false)}>Why Us?</NavLink></li>
-        <li><NavLink to="/career" onClick={() => setIsOpen(false)}>Career</NavLink></li>
+      <ul className="nav-list">
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/why">Why Us?</NavLink></li>
+        <li><NavLink to="/career">Career</NavLink></li>
       </ul>
     </nav>
   );
